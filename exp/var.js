6a6475edad76701bbe56f8627f66b347c147f220
joshkenney <joshuagkenney@gmail.com>
2020-09-30 19:51:36 -0400

    renamed stim added key_press

--- exp/var.js
+++ exp/var.js
@@ -21,17 +21,17 @@ let unaltered_90 = [];
 
 let intel_stim=[];
     for (let i = 0; i < intel.length ; i++){
-        intel_stim.push({stimulus: intel_45[i], data: {test_part: 'intel', stimulus: intel[i]+".wav"}});
+        intel_stim.push({stimulus: intel_45[i], data: {test_part: 'intel', stim: intel[i]+".wav"}});
     }
 
 let unintel_stim=[];
     for (let i = 0; i < unintel.length ; i++){
-        unintel_stim.push({stimulus: unintel_45[i], data: {test_part: 'unintel', stimulus: unintel[i]+".wav"}});
+        unintel_stim.push({stimulus: unintel_45[i], data: {test_part: 'unintel', stim: unintel[i]+".wav"}});
     }
 
 let unaltered_stim=[];
     for (let i = 0; i < unaltered.length ; i++){
-        unaltered_stim.push({stimulus: unaltered_90[i], data: {test_part: 'unaltered', stimulus: unaltered[i]+".wav"}});
+        unaltered_stim.push({stimulus: unaltered_90[i], data: {test_part: 'unaltered', stim: unaltered[i]+".wav"}});
     }
 
 let full_stim = intel_stim.concat(unintel_stim);
