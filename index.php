4c5e99c33e7d76b87b3be984bf10bb2b420c4abc
joshkenney <joshuagkenney@gmail.com>
2021-01-09 02:08:02 -0500

    jQuery -> 3.5.1, jQuery API

--- index.php
+++ index.php
@@ -33,6 +33,7 @@ $ageInMonths = $_GET["interview_age"];
   <head>
     <title>Sine Wave Speech</title>  <!-- This is for changing the title -->
     <script src="db/validate.js"></script>
+    <script src="js/jquery-3.5.1.min.js"></script>
     <script src="jsPsych/jspsych.js"></script>
       <script src="jsPsych/plugins/jspsych-audio-keyboard-response.js"></script>
       <script src="jsPsych/plugins/jspsych-html-keyboard-response.js"></script> 
@@ -40,10 +41,19 @@ $ageInMonths = $_GET["interview_age"];
       <link  href="css/style.css" rel="stylesheet" type="text/css" > <!--the interpreter will take care of ordering, rel, type, href do not have to be in a specific order -->
   </head>
   <body id='unload' onbeforeunload="return areYouSure()" style="background-color:light-grey;">  <!--any time you see style = all properties that follow are inline css -->
-    <?php include_once 'include/nda.php';?>
+    <?php
+      if ($db_connection_status == true) {
+        include_once "include/nda.php";
+        // echo'<br>';
+        // echo'connected';
+      } else if ($db_connection_status == false) {
+        include_once "include/intake.php";
+        // echo'<br>';
+        // echo'not connected';
+      }
+    ?>
   </body>
   <footer>
-    <script src="js/jquery-git.js"></script>
     <script src="exp/conf.js"></script>
     <script src="exp/fn.js"></script>
     <script src="exp/var.js"></script>
