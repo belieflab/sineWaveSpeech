90d09b708a508f6642809e6207e768a30c728692
joshkenney <joshuagkenney@gmail.com>
2021-01-17 19:56:20 -0500

    relocated db/config.php to [DOCUMENT_ROOT] . '/config.php' to address github security vulerability

--- index.php
+++ index.php
@@ -1,30 +1,6 @@
 <?php
-$post_data = json_decode(file_get_contents('php://input'), true); 
-// the directory "data" must be writable by the server
-$name = "data/".$post_data['filename'].".csv"; 
-$data = $post_data['filedata'];
-// write the file to disk
-file_put_contents($name, $data);
-
-include_once ("db/config.php");
-
-$studyId = $_GET["studyId"];
-$candidateId = $_GET["candidateId"];
-if (isset($candidateId)) {
-  $query = "SELECT GUID from phi where sub_id = $candidateId";
-  $prepare = $db_connection->prepare($query);
-  $prepare->execute();
-  $result = $prepare->get_result();
-  $row = $result->fetch_assoc();
-  $guid = $row["GUID"];
-  $prepare->close();
-  } else {
-}
-$subjectKey = $_GET["subjectkey"];
-$consortId = $_GET["src_subject_id"];
-$sexAtBirth = $_GET["sex"];
-$institutionAlias = $_GET["site"];
-$ageInMonths = $_GET["interview_age"];
+  require_once 'db/data.php';
+  require_once 'db/config.php';
 ?>
 
 
