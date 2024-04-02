9e8721805591e1f0517c6565e50050cac26c92ca
joshkenney <joshuagkenney@gmail.com>
2024-04-02 16:25:19 -0400

    set debug to false

--- exp/conf.js
+++ exp/conf.js
@@ -6,7 +6,7 @@
 
 // Debug Mode
 // Options: true, false
-let debug = true; // Default debug mode setting for the experiment
+let debug = false; // Default debug mode setting for the experiment
 
 // Experiment Name
 const experimentName = "Sine Wave Speech"; // Name displayed in the browser title bar
